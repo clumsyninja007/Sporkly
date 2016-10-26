22c0925d55b3c2e4edc619a8b14e1784c7fd583d
clumsyninja007 <clumsyninja007@gmail.com>
2016-10-26 10:25:56 -0700

    Working main buttons

--- app/src/main/java/com/example/aharm/sporkly/MainActivity.java
+++ app/src/main/java/com/example/aharm/sporkly/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.aharm.sporkly;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button recipe, pantry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        recipe = (Button)findViewById(R.id.myIngredients);
+        pantry = (Button)findViewById(R.id.myPantry);
+
+        recipe.setOnClickListener(this);
+        pantry.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.myIngredients:
+                startActivity(new Intent(MainActivity.this, myIngredientsActivity.class));
+                break;
+            case R.id.myPantry:
+                startActivity(new Intent(MainActivity.this, myPantryActivity.class));
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/aharm/sporkly/myIngredientsActivity.java
@@ -0,0 +1,16 @@
+package com.example.aharm.sporkly;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by aharm on 10/26/2016.
+ */
+
+public class myIngredientsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_ingredients);
+    }
+}
