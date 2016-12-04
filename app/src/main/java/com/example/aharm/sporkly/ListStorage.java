f7e2e7c38f633038b287efac310e0dc098c4066a
David <dsides94@gmail.com>
2016-12-04 10:23:43 -0800

    Added comments to ListStorage.

--- app/src/main/java/com/example/aharm/sporkly/ListStorage.java
+++ app/src/main/java/com/example/aharm/sporkly/ListStorage.java
@@ -11,7 +11,8 @@ import java.util.Scanner;
 
 /**
  * This class is used to save/load data from a file, to be displayed
- * in a ListView.
+ * in a ListView. It stores data using json, every entry into the file
+ * is loaded as a JSONObject.
  */
 
 class ListStorage {
@@ -25,18 +26,35 @@ class ListStorage {
         this.fileName = fileName;
     }
 
+    /*
+     * Sets the file that will be saved or loaded from.
+     */
     void setFile (String fileName) {
         this.fileName = fileName;
     }
 
+    /*
+     * Gets the Array for the display text items, this is what will
+     * be used when creating an array adapter, so that the ListView
+     * using that adapter can show the strings within the items list.
+     */
     ArrayList<String> getItems () {
         return items;
     }
 
+    /*
+     * Gets a JSONObject stored within this list storage. Each entry into
+     * this list storage is stored as a JSONObject.
+     */
     JSONObject getData (int index) {
         return data.get(index);
     }
 
+    /*
+     * Updates the items list array with the "test" property of each JSONObject,
+     * so they can be displayed in a ListView. Example of what would be stored in this
+     * property is a recipe name.
+     */
     void refresh() {
         items.clear();
         for (JSONObject obj : data) {
@@ -48,6 +66,10 @@ class ListStorage {
         }
     }
 
+    /*
+     * Adds an entry to this list storage, creating the JSONObject internally.
+     * @param text the text that will be displayed in the ListView.
+     */
     boolean add(String text) {
         try{
             JSONObject obj = new JSONObject();
@@ -69,6 +91,10 @@ class ListStorage {
         return false;
     }
 
+    /*
+     * Adds an entry to this list storage, using a pre create JSONObject
+     * @param objAdd the JSONObject to add to the list storage.
+     */
     boolean add(JSONObject objAdd) {
         try{
             JSONObject obj = new JSONObject(objAdd.toString());
@@ -88,6 +114,14 @@ class ListStorage {
         return false;
     }
 
+    /*
+     * Adds an entry to this list storage, allowing you to specify display text
+     * and also a JSONObject that can contain more data. Exmaple would be the favorites
+     * storage, which stores the recipe name as the text, and the JSONObject contains
+     * the id, so it can be sent to the API.
+     * @param text the text
+     * @param objAdd the JSONObject to add additional properties.
+     */
     boolean add(String text, JSONObject objAdd) {
         try{
             JSONObject obj = new JSONObject(objAdd.toString());
@@ -109,6 +143,14 @@ class ListStorage {
         return false;
     }
 
+    /*
+     * Looks to see if an integer value with a specific name exists
+     * in any of the stored JSONObjects, such as an id, to see if we already
+     * have that recipe id stored.
+     * @param name the name of the property.
+     * @param value the value that will be looked for.
+     * @return the index of the property if found, it will return -1 if not found.
+     */
     int findInt(String name, int value) {
         for (int i = 0; i < data.size(); i++) {
             try{
@@ -124,13 +166,21 @@ class ListStorage {
         return -1;
     }
 
+    /*
+     * Removes an entry from the list storage.
+     * @param index the index to remove.
+     */
     JSONObject remove(int index) {
-        JSONObject obj = data.remove(index);
+        if (index < data.size()) {
+            JSONObject obj = data.remove(index);
 
-        refresh();
-        save();
+            refresh();
+            save();
 
-        return obj;
+            return obj;
+        } else {
+            return null;
+        }
     }
 
     boolean save() {
