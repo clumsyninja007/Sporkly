a56c498ac5e0f0ea0805d448b04c0bccbf79c0f6
clumsyninja007 <clumsyninja007@gmail.com>
2016-12-07 17:10:54 -0800

    Working on myIngredients

--- app/src/main/java/com/example/aharm/sporkly/MyIngredientsActivity.java
+++ app/src/main/java/com/example/aharm/sporkly/MyIngredientsActivity.java
@@ -1,8 +1,10 @@
 package com.example.aharm.sporkly;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -34,6 +36,8 @@ import java.util.ArrayList;
 public class MyIngredientsActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener, TextView.OnEditorActionListener {
     MyApplication app;
 
+    ListStorage pantryStorage;
+
     ListView recipeList;
     Button recipeButton;
     EditText recipeText;
@@ -51,6 +55,8 @@ public class MyIngredientsActivity extends AppCompatActivity implements View.OnC
 
         app = (MyApplication)this.getApplication();
 
+        pantryStorage = app.getPantryStorage();
+
         recipeButton = (Button)findViewById(R.id.recipeButton);
         recipeText = (EditText)findViewById(R.id.recipeText);
         recipeList = (ListView)findViewById(R.id.recipeList);
@@ -63,10 +69,14 @@ public class MyIngredientsActivity extends AppCompatActivity implements View.OnC
         recipeButton.setOnClickListener(this);
         recipeList.setOnItemClickListener(this);
         recipeText.setOnEditorActionListener(this);
+
+
+        pantryAlert();
     }
 
     @Override
     public void onClick(View view) {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
         switch(view.getId()) {
             case R.id.recipeButton:
                 Log.i("recipeButton", "clicked");
@@ -75,8 +85,14 @@ public class MyIngredientsActivity extends AppCompatActivity implements View.OnC
 
                 // This forces the keyboard to hide, because for some reason
                 // the keyboard showing makes the search results not show.
-                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+
                 imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                break;
+            case R.id.pantryButton:
+                Log.i("pantryButton", "clicked");
+
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
                 break;
             default:
                 break;
@@ -153,5 +169,68 @@ public class MyIngredientsActivity extends AppCompatActivity implements View.OnC
                 Log.d("http", "API request failed");
             }
         }
+    }
+
+    public void pantryAlert(){
+
+        // where we will store or remove selected items
+        final ArrayList<Integer> mSelectedItems = new ArrayList<Integer>();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MyIngredientsActivity.this);
+
+        // set the dialog title
+        builder.setTitle("Choose One or More")
+
+                // specify the list array, the items to be selected by default (null for none),
+                // and the listener through which to receive call backs when items are selected
+                // R.array.choices were set in the resources res/values/strings.xml
+                .setMultiChoiceItems(R.array.choices, null, new DialogInterface.OnMultiChoiceClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+
+                        if (isChecked) {
+                            // if the user checked the item, add it to the selected items
+                            mSelectedItems.add(which);
+                        }
+
+                        else if (mSelectedItems.contains(which)) {
+                            // else if the item is already in the array, remove it
+                            mSelectedItems.remove(Integer.valueOf(which));
+                        }
+
+                        // you can also add other codes here,
+                        // for example a tool tip that gives user an idea of what he is selecting
+                        // showToast("Just an example description.");
+                    }
+
+                })
+
+                // Set the action buttons
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        // user clicked OK, so save the mSelectedItems results somewhere
+                        // here we are trying to retrieve the selected items indices
+                        String selectedIndex = "";
+                        for(Integer i : mSelectedItems){
+                            selectedIndex += i + ", ";
+                        }
+
+                        showToast("Selected index: " + selectedIndex);
+
+                    }
+                })
+
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // removes the AlertDialog in the screen
+                    }
+                })
+
+                .show();
+
     }
 }
